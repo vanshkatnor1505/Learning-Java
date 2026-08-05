16e6d558b16ea201f624c3ae02399531e8174fae
Vansh <vanshkatnor1737@gmail.com>
2026-08-05 20:58:38 +0530

    data types in JAVA practice question 5

---
+++ datatypes/Ques5.java
@@ -0,0 +1,18 @@
+package datatypes;
+
+public class Ques5 {
+    public static void main(String[] args) {
+        String name = "Vansh";
+        int age = 19;
+        char grade = 'A';
+        double height = 175.5;
+        boolean passed = true;
+        System.out.println("========== Student Profile ==========");
+        System.out.println("Name   : " + name);
+        System.out.println("Age    : " + age);
+        System.out.println("Grade  : " + grade);
+        System.out.println("Height : " + height);
+        System.out.println("Passed : " + passed);
+        System.out.println("=====================================");
+    }
+}
