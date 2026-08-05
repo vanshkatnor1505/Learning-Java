09ea8bdd82f3dec8e95e4d5d00a6f09a287be039
Vansh <vanshkatnor1737@gmail.com>
2026-08-05 19:38:00 +0530

    vairables in JAVA parctice question 4

---
+++ variables/Ques4.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class Ques4 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
