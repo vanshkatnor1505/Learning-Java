f05ae0d95f630f7f3b471700f09133f5a1f58811
Vansh <vanshkatnor1737@gmail.com>
2026-08-05 20:58:04 +0530

    data types in JAVA practice question 3

---
+++ datatypes/Ques3.java
@@ -0,0 +1,17 @@
+package datatypes;
+
+public class Ques3 {
+    public static void main(String[] args) {
+        int age = 19;
+        double num = 175.5;
+        char letter = 'A';
+        boolean learning = true;
+        String name = "Vansh";
+        System.out.println(age);
+        System.out.println(num);
+        System.out.println(letter);
+        System.out.println(learning);
+        System.out.println(name);
+
+    }
+}
