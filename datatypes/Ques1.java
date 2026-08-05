73f42303c6ae7184fddac3e2103a79306bf376c0
Vansh <vanshkatnor1737@gmail.com>
2026-08-05 20:57:40 +0530

    data types in JAVA practice question 1

---
+++ datatypes/Ques1.java
@@ -0,0 +1,10 @@
+package datatypes;
+
+public class Ques1 {
+    public static void main(String[] args) {
+        String name = "Vansh";
+        int age = 19;
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
