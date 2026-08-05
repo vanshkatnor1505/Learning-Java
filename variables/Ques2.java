4fb4096fcf41828ce71b01e6ae71d75171d0b8fc
Vansh <vanshkatnor1737@gmail.com>
2026-08-05 19:37:40 +0530

    vairables in JAVA parctice question 2

---
+++ variables/Ques2.java
@@ -0,0 +1,11 @@
+package variables;
+
+public class Ques2 {
+
+    public static void main(String[] args) {
+        String name = "Vanshdeep Katnor";
+        String city = "Hoshiarpur";
+        System.out.println(name);
+        System.out.println(city);
+    }
+}
