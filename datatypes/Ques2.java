6a2225576253a2efd261c5f5856a0084480b6332
Vansh <vanshkatnor1737@gmail.com>
2026-08-05 20:57:52 +0530

    data types in JAVA practice question 2

---
+++ datatypes/Ques2.java
@@ -0,0 +1,10 @@
+package datatypes;
+
+public class Ques2 {
+    public static void main(String[] args) {
+        double height = 175.5;
+        boolean student = true;
+        System.out.println(height);
+        System.out.println(student);
+    }
+}
