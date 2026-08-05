5222a08408714b74164ae20c7c12a9508de32b7d
Vansh <vanshkatnor1737@gmail.com>
2026-08-05 20:58:19 +0530

    data types in JAVA practice question 4

---
+++ datatypes/Ques4.java
@@ -0,0 +1,12 @@
+package datatypes;
+
+public class Ques4 {
+    public static void main(String[] args) {
+        String language = "Java";
+        int years = 1;
+        boolean fun = true;
+        System.out.println("Favorite Language : "  + language);
+        System.out.println("Years Programming : " + years);
+        System.out.println("Enjoy Coding : " + fun);
+    }
+}
