b87e43e3270a1c14a05522f5b2e4697fda750ae8
Vansh <vanshkatnor1737@gmail.com>
2026-08-05 19:38:11 +0530

    vairables in JAVA parctice question 5

---
+++ variables/Ques5.java
@@ -0,0 +1,21 @@
+package variables;
+
+public class Ques5 {
+    public static void main(String[] args) {
+        String name = "Vansh";
+        int age = 19;
+        String city = "Hoshiarpur";
+        System.out.print("Name : ");
+        System.out.println(name);
+        System.out.print("Age : ");
+        System.out.println(age);
+        System.out.print("City : ");
+        System.out.println(city);
+
+        // ALTERNATIVE
+        // System.out.println("Name: " + name);
+        // System.out.println("Age: " + age);
+        // System.out.println("City: " + city);
+    }
+
+}
