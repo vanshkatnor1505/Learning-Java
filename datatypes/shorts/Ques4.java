0251a156e4b7dc55bd4973832077c38aa6a04c6a
Vansh <vanshkatnor1737@gmail.com>
2026-08-06 20:05:51 +0530

    learning about short datatype deeply through practice questions practice question 4

---
+++ datatypes/shorts/Ques4.java
@@ -0,0 +1,13 @@
+package datatypes.shorts;
+
+public class Ques4 {
+    public static void main(String[] args) {
+        short a = 1000;
+        short b = 2000;
+        short c = 3000;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
