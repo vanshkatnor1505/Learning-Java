cf5b8ccc1cdc3d1400eb424e795fd1a1e30505e2
Vansh <vanshkatnor1737@gmail.com>
2026-08-06 20:05:35 +0530

    learning about short datatype deeply through practice questions practice question

---
+++ datatypes/shorts/Ques2.java
@@ -0,0 +1,11 @@
+package datatypes.shorts;
+
+public class Ques2 {
+    public static void main(String[] args) {
+        short population = 25000;
+        short distance = 15000;
+
+        System.out.println(population);
+        System.out.println(distance);
+    }
+}
