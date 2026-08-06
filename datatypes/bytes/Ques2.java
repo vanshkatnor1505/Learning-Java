c18c40790cf65fb691ed408f11182da1f2c9a409
Vansh <vanshkatnor1737@gmail.com>
2026-08-06 19:13:45 +0530

    learning about byte datatype deeply through practice questions practice question 2

---
+++ datatypes/bytes/Ques2.java
@@ -0,0 +1,10 @@
+package datatypes.bytes;
+
+public class Ques2 {
+    public static void main(String[] args) {
+        byte lives = 3;
+        byte level = 25;
+        System.out.println(lives);
+        System.out.println(level);
+    }
+}
