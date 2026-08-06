48d0de03c93dbe4a12d5155a0b796f17075b85a1
Vansh <vanshkatnor1737@gmail.com>
2026-08-06 20:05:44 +0530

    learning about short datatype deeply through practice questions practice question 3

---
+++ datatypes/shorts/Ques3.java
@@ -0,0 +1,8 @@
+package datatypes.shorts;
+
+public class Ques3 {
+    public static void main(String[] args) {
+        short temperature = -250;
+        System.out.println(temperature);
+    }
+}
