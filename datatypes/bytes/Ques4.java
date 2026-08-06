5dd412e5555d7171169ecbed129908431a8fec88
Vansh <vanshkatnor1737@gmail.com>
2026-08-06 19:14:06 +0530

    learning about byte datatype deeply through practice questions practice question 4

---
+++ datatypes/bytes/Ques4.java
@@ -0,0 +1,12 @@
+package datatypes.bytes;
+
+public class Ques4 {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+        byte c = 30;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
