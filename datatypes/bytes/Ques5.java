7241498eb969a4d7ea62bb423368646216be7738
Vansh <vanshkatnor1737@gmail.com>
2026-08-06 19:14:14 +0530

    learning about byte datatype deeply through practice questions practice question 5

---
+++ datatypes/bytes/Ques5.java
@@ -0,0 +1,14 @@
+package datatypes.bytes;
+
+public class Ques5 {
+    public static void main(String[] args) {
+        byte lives = 5;
+        byte level = 18;
+        byte coins = 99;
+        System.out.println("===== Player Stats =====");
+        System.out.println("Lives : " + lives);
+        System.out.println("Level : " + level);
+        System.out.println("Coins : " + coins);
+        System.out.println("========================");
+    }
+}
