d9edfd70f9cb6a033081ace5cd5cd6181e102d7a
Vansh <vanshkatnor1737@gmail.com>
2026-08-06 20:06:01 +0530

    learning about short datatype deeply through practice questions practice question 5

---
+++ datatypes/shorts/Ques5.java
@@ -0,0 +1,17 @@
+package datatypes.shorts;
+
+public class Ques5 {
+
+    public static void main(String[] args) {
+        short students = 1250;
+        byte teachers = 85;
+        byte classrooms = 40;
+
+        System.out.println("===== School Information =====");
+        System.out.println("Students  : " + students);
+        System.out.println("Teachers  : " + teachers);
+        System.out.println("Classrooms : " + classrooms);
+        System.out.println("==============================");
+    }
+
+}
