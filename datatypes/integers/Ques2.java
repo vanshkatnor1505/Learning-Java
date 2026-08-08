1a6b1453111fdf78e3b065e26ac5812ebea3d818
Vansh <vanshkatnor1737@gmail.com>
2026-08-08 19:01:11 +0530

    learned about integer data type in java through practice questions

---
+++ datatypes/integers/Ques2.java
@@ -0,0 +1,29 @@
+// ========== Employee ==========
+// Name                : Vansh
+// Employee ID         : 10245
+// Department          : Software Development
+// Salary              : 85000
+// Years of Experience : 3
+// ==============================
+
+
+package datatypes.integers;
+
+public class Ques2 {
+    public static void main(String[] args) {
+        String name = "Vansh";
+        int id = 10245;
+        String department = "Software Development";
+        int salary = 85000;
+        byte experience = 3;
+
+
+        System.out.println("========== Employee ==========");
+        System.out.println("Name                : " + name);
+        System.out.println("Employee ID         : " + id);
+        System.out.println("Department          : " + department);
+        System.out.println("Salary              : " + salary);
+        System.out.println("Years of experience : " + experience);
+        System.out.println("==============================");
+    }
+}
