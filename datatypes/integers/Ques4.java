2fe7f88e58de1d42e3cf347243241d9002b48f81
Vansh <vanshkatnor1737@gmail.com>
2026-08-08 19:02:15 +0530

    learned about integer data type in java through practice questions

---
+++ datatypes/integers/Ques4.java
@@ -0,0 +1,23 @@
+package datatypes.integers;
+
+public class Ques4 {
+    public static void main(String[] args) {
+        String name = "Patient 1";
+        int id = 100245;
+        byte age = 19;
+        int roomno = 205;
+        char blood = 'O';
+        boolean admitted = true;
+        int bill = 125000;
+
+        System.out.println("========== Hospital Record ==========");
+        System.out.println("Patient Name    : " + name);
+        System.out.println("Patient ID      : " + id);
+        System.out.println("Age             : " + age);
+        System.out.println("Room Number     : " + roomno);
+        System.out.println("Blood Group     : " + blood + "+");
+        System.out.println("Admitted        : " + admitted);
+        System.out.println("Hospital Bill   : " + bill);
+        System.out.println("=====================================");
+    }
+}
