8adc4f479877d92e85e3cb607817339bd52bad12
Vansh <vanshkatnor1737@gmail.com>
2026-08-08 19:00:48 +0530

    learned about integer data type in java through practice questions

---
+++ datatypes/integers/Ques1.java
@@ -0,0 +1,21 @@
+// ========== Bank Account ==========
+// Account Holder : Vansh
+// Account Number : 12345678
+// Balance        : 50000
+// ==================================
+
+package datatypes.integers;
+
+public class Ques1 {
+    public static void main(String[] args) {
+        String name = "Vansh";
+        int accnumber = 12345678;
+        int balance = 50000;
+
+        System.out.println("========== Bank Account ==========");
+        System.out.println("Account Holder : " + name);
+        System.out.println("Account Number : " + accnumber);
+        System.out.println("Balance        : " + balance);
+        System.out.println("==================================");
+    }
+}
