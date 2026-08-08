903051b508934c41b905e012a404c89eed288d88
Vansh <vanshkatnor1737@gmail.com>
2026-08-08 19:02:26 +0530

    learned about integer data type in java through practice questions

---
+++ datatypes/integers/Ques5.java
@@ -0,0 +1,25 @@
+package datatypes.integers;
+
+public class Ques5 {
+    public static void main(String[] args) {
+        String name = "Chandrayan";
+        int id = 151515;
+        byte astronauts = 6;
+        int fuel = 25000;
+        int duration = 18;
+        boolean success = true;
+        char grade = 'A';
+
+        System.out.println(" =========================================");
+        System.out.println("         SPACE MISSION DASHBOARD          ");
+        System.out.println("==========================================");
+        System.out.println("Mission Name       : " + name);
+        System.out.println("Mission ID         : " + id);
+        System.out.println("Astronauts         : " + astronauts);
+        System.out.println("Fuel Units         : " + fuel);
+        System.out.println("Duration (Days)    : " + duration);
+        System.out.println("Launch Successful  : " + success);
+        System.out.println("Mission Grade      : " + grade);
+        System.out.println("=========================================");
+    }
+}
