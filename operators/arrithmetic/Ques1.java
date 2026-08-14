cd4857b3de21adf30b2e9e080b11032500700d07
Vansh <vanshkatnor1737@gmail.com>
2026-08-14 15:28:02 +0530

    leraning artihmetic operators in java through practice questions (Question 1)

---
+++ operators/arrithmetic/Ques1.java
@@ -0,0 +1,20 @@
+package operators.arrithmetic;
+
+
+public class Ques1 {
+    public static void main(String[] args) {
+        int notebook = 50;
+        int pen = 20;
+        int bag = 500;
+        int total;
+        total = notebook + pen + bag;
+
+        System.out.println("========== BILL ==========");
+        System.out.println("Notebook : ₹" + notebook);
+        System.out.println("Pen      : ₹" + pen);
+        System.out.println("Bag      : ₹" + bag);
+        System.out.println("--------------------------");
+        System.out.println("Total    : ₹" + total);
+        System.out.println("==========================");
+    }
+}
