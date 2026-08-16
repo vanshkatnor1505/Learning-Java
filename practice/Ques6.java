7f916d96730378baab8961c35b13bc5db228222b
Vansh <vanshkatnor1737@gmail.com>
2026-08-16 17:32:35 +0530

    learning java through various different applications of the java using different variables and methods (Question 6)

--- practice/Ques6.java
+++ practice/Ques6.java
@@ -1,5 +1,13 @@
+// the answer will be float 
 package practice;
 
 public class Ques6 {
-    
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 2;
+        int c = 2;
+
+        float d = (a+b+c)/3.0f;
+        System.out.println(d);
+    }
 }
