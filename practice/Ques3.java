7b0a0ce0c638e8f9f36bab070553c7c9431e3b73
Vansh <vanshkatnor1737@gmail.com>
2026-08-16 17:20:09 +0530

    learning java through various different applications of the java using different variables and methods (Question 3)

---
+++ practice/Ques3.java
@@ -0,0 +1,12 @@
+// Program to check size of any data type in java
+
+package practice;
+
+public class Ques3 {
+    public static void main(String[] args) {
+        // size in bits
+        System.out.println(Integer.SIZE);
+        // size in bytes
+        System.out.println(Integer.BYTES);
+    }
+}
