77eb90b44999c0dbaca3f1cffea724f020c2fa8c
Vansh <vanshkatnor1737@gmail.com>
2026-08-16 17:20:25 +0530

    learning java through various different applications of the java using different variables and methods (Question 4)

---
+++ practice/Ques4.java
@@ -0,0 +1,17 @@
+// Program to get an intege as an input
+
+package practice;
+import java.util.*;
+
+public class Ques4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("ENTER YOU NUMBER : ");
+        int num = sc.nextInt();
+        sc.close();
+
+        System.out.println(num);
+    }
+}
