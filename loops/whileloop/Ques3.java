8ca40ded8f153f2e4d908a7d97a884a17cd935d6
Vansh <vanshkatnor1737@gmail.com>
2026-08-21 15:01:52 +0530

    learning loops in java specifically while loop through differnt practice questions which include use of previous concepts i had learned so far (Question 1)

---
+++ loops/whileloop/Ques3.java
@@ -0,0 +1,28 @@
+// Digit Processing
+
+// GIVEN ::::
+// int number = 58372;
+
+// Using a while loop, calculate:
+
+// The number of digits
+// The sum of all digits
+
+package loops.whileloop;
+
+public class Ques3 {
+    public static void main(String[] args) {
+        int number =  58372;
+        int count = 0;
+        int sum = 0;
+
+        while (number !=0) {
+            int digit = number % 10;
+            sum += digit;
+            count ++;
+            number = number / 10;
+        }
+        System.out.println("COUNT : " + count);
+        System.out.println("SUM   : " + sum);
+    }    
+}
