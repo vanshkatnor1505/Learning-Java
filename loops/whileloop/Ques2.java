7ec3f3eba40c8ad76ac0e3b55b81f149f6227120
Vansh <vanshkatnor1737@gmail.com>
2026-08-21 15:01:45 +0530

    learning loops in java specifically while loop through differnt practice questions which include use of previous concepts i had learned so far (Question 1)

---
+++ loops/whileloop/Ques2.java
@@ -0,0 +1,22 @@
+// SUM of n numbers
+
+// GIVEN ::::
+// int number = 1;
+// int sum = 0;
+
+// Using only a while loop, calculate the sum of all integers from 1 through 10.
+
+package loops.whileloop;
+
+public class Ques2 {
+    public static void main(String[] args) {
+        int number = 1;
+        int sum = 0;
+
+        while (number <= 10) {
+            sum += number;
+            number ++;
+        }
+        System.out.println("SUM IS : " + sum);
+    }
+}
