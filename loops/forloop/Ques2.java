e07bd4978f67b3f70e1bde981a959b4234ece5a0
Vansh <vanshkatnor1737@gmail.com>
2026-08-24 19:36:02 +0530

    learning loops in java specifically for loop through different practice questions which also include use of previous concepts i had learned so far (Question 2)
    part 1 perfect numbers done
    part 2 prime numbers done
    part 3 Armstrong number done
    part 4 digit intelligence done

--- loops/forloop/Ques2.java
+++ loops/forloop/Ques2.java
@@ -17,13 +17,11 @@
 // 28
 // 496
 
-
 // Part 2 — Prime Numbers
 // Find the total number of prime numbers between 1 and 1000.
 // You don't need to print every prime.
 // Print:
 // TOTAL PRIMES : xxx
-
 
 // Part 3 — Armstrong Numbers
 // Find every 3-digit Armstrong number between 100 and 999.
@@ -44,7 +42,6 @@
 // 371
 // 407
 
-
 // Part 4 — Digit Intelligence
 // For every number from 1 to 1000, determine its digit sum.
 // Find:
@@ -53,14 +50,12 @@
 // LARGEST DIGIT SUM NUMBER : xxx
 // DIGIT SUM                : xxx
 
-
 // B. Number with the smallest digit sum
 // Print:
 // SMALLEST DIGIT SUM NUMBER : xxx
 // DIGIT SUM                 : xxx
 
 // If multiple numbers have the same sum, keep the first one encountered.
-
 
 // Part 5 — The Final Classification ☠️
 // Now find numbers between 1 and 1000 that satisfy ALL of these:
@@ -80,11 +75,89 @@
 // SPECIAL NUMBERS:
 // TOTAL SPECIAL NUMBERS : xxx
 
-
-
-
 package loops.forloop;
 
 public class Ques2 {
-    
+    public static void main(String[] args) {
+        int count = 0;
+        int largest = 0;
+        int smallest = 99;
+        // Perfect numbers
+        for (int i = 1; i <= 1000; i++) {
+        int sum = 0;
+
+        for (int j = 1; j < i; j++) {
+        if (i % j == 0) {
+        sum += j;
+        }
+        }
+
+        if (sum == i) {
+        System.out.println(i + " is perfect");
+        }
+        }
+
+        // Prime numbers
+        for (int l = 1; l <= 1000; l++) {
+
+        boolean isPrime = true;
+
+        if (l < 2) {
+        isPrime = false;
+        }
+
+        for (int k = 2; k * k <= l; k++) {
+        if (l % k == 0) {
+        isPrime = false;
+        break;
+        }
+        }
+
+        if (isPrime) {
+        count++;
+        }
+
+        }
+        System.out.println(count);
+
+        for (int m = 100; m < 1000; m++) {
+
+            int temp = m;
+            int Armstrongsum = 0;
+
+            for (int i = 1; i < 4; i++) {
+                int digit = temp % 10;
+                Armstrongsum += digit * digit * digit;
+                temp /= 10;
+            }
+
+            if (Armstrongsum == m) {
+                System.out.println(m);
+            }
+        }
+
+        for (int i = 1; i <= 1000; i++) {
+            int temp = i;
+            int digitsum = 0;
+
+            for (int j = 1; j < 5; j++) {
+                int digit = temp % 10;
+                digitsum += digit;
+                temp /= 10;
+            }
+
+            if (digitsum > largest) {
+                largest = digitsum;
+            }
+            if (digitsum < smallest) {
+                smallest = digitsum;
+            }
+
+        }
+        System.out.println("LARGEST" + largest);
+        System.out.println("smallest " + smallest);
+
+
+        // final classification
+    }
 }
