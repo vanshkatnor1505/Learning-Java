02eeadbb2838856b86309e2c7e2af51757988de1
Vansh <vanshkatnor1737@gmail.com>
2026-08-26 18:19:00 +0530

    added proper comments to each practice question for better understanding and readability. (Ques1)

--- datatypes/Ques1.java
+++ datatypes/Ques1.java
@@ -1,10 +1,16 @@
+// Store name and age in a variable and print them 
+
+// EXPECTED OUTPUT 
+// NAME : vansh
+// AGE  : 19
+
 package datatypes;
 
 public class Ques1 {
     public static void main(String[] args) {
         String name = "Vansh";
         int age = 19;
-        System.out.println(name);
-        System.out.println(age);
+        System.out.println("NAME : " + name);
+        System.out.println("AGE  : " + age);
     }
 }
