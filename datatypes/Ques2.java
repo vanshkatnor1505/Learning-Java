96e3836e7f45cfd9012c16c92dadd4a323dc49cf
Vansh <vanshkatnor1737@gmail.com>
2026-08-26 18:19:12 +0530

    added proper comments to each practice question for better understanding and readability. (Ques2)

--- datatypes/Ques2.java
+++ datatypes/Ques2.java
@@ -1,10 +1,16 @@
+// Store height and stdent = true in a suitable variable and print them 
+
+// EXPECTED OUTPUT 
+// HEIGHT   : 175.5
+// STUDENT  : true
+
 package datatypes;
 
 public class Ques2 {
     public static void main(String[] args) {
         double height = 175.5;
         boolean student = true;
-        System.out.println(height);
-        System.out.println(student);
+        System.out.println("HEIGHT  : " +  height);
+        System.out.println("STUDENT : " + student);
     }
 }
