fe5c2d17d8665fd0ad3cc04e514be7c64498b0d2
Vansh <vanshkatnor1737@gmail.com>
2026-08-26 18:19:20 +0530

    added proper comments to each practice question for better understanding and readability. (Ques3)

--- datatypes/Ques3.java
+++ datatypes/Ques3.java
@@ -1,17 +1,27 @@
+// Store folowing in a suitable variable and print them 
+// age, height, letter, learning(boolean) , name
+
+// EXPECTED OUTPUT 
+// AGE      : 19
+// HEIGHT   : 175.5
+// LETTER   : A
+// LEARNING : true
+// NAME     : Vansh
+
 package datatypes;
 
 public class Ques3 {
     public static void main(String[] args) {
         int age = 19;
-        double num = 175.5;
+        double height = 175.5;
         char letter = 'A';
         boolean learning = true;
         String name = "Vansh";
-        System.out.println(age);
-        System.out.println(num);
-        System.out.println(letter);
-        System.out.println(learning);
-        System.out.println(name);
+        System.out.println("AGE      : " + age);
+        System.out.println("HEIGHT   : " + height);
+        System.out.println("LETTER   : " + letter);
+        System.out.println("LEARNING : " + learning);
+        System.out.println("NAME     : " + name);
 
     }
 }
