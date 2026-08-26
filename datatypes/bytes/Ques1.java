41e3aee0ff2c79eeec8d076873c512de81130cd2
Vansh <vanshkatnor1737@gmail.com>
2026-08-26 18:41:04 +0530

    added proper comments to each practice question for better understanding and readability. (Ques1)

--- datatypes/bytes/Ques1.java
+++ datatypes/bytes/Ques1.java
@@ -1,8 +1,13 @@
+// make a byte variable to store age
+
+// EXPECTED OUTPUT 
+// AGE : 19
+
 package datatypes.bytes;
 
 public class Ques1 {
     public static void main(String[] args) {
         byte age = 19;
-        System.out.println(age);
+        System.out.println("AGE : " + age);
     }
 }
