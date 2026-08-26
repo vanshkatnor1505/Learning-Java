1e496c1ef008869f1e3e392c2b133c254e867b4a
Vansh <vanshkatnor1737@gmail.com>
2026-08-26 18:41:22 +0530

    added proper comments to each practice question for better understanding and readability. (Ques2)

--- datatypes/bytes/Ques2.java
+++ datatypes/bytes/Ques2.java
@@ -1,10 +1,16 @@
+// make byte variable to store lives and level 
+
+// EXPECTED OUTPUT
+// LIVES : 3
+// LEVEL : 25
+
 package datatypes.bytes;
 
 public class Ques2 {
     public static void main(String[] args) {
         byte lives = 3;
         byte level = 25;
-        System.out.println(lives);
-        System.out.println(level);
+        System.out.println("LIVES : " + lives);
+        System.out.println("LEVEL : " + level);
     }
 }
