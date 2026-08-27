598cf6ad8fea4aa64df75f4ed9360cac9c811c00
Vansh <vanshkatnor1737@gmail.com>
2026-08-27 19:02:57 +0530

    learning loops in java specifically for loop through different practice questions which also include use of previous concepts i had learned so far (Question 9)

--- loops/forloop/Ques9.java
+++ loops/forloop/Ques9.java
@@ -23,5 +23,20 @@
 package loops.forloop;
 
 public class Ques9 {
-        
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i ++) {
+            for (int j = 1; j <= i; j ++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= n; i ++) {
+            for (int j = 1; j <= n - i; j ++) {
+                System.out.print(n-i);
+            }
+            System.out.println();
+        }
+    }
+
 }
