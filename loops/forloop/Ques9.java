e1777ef9e49f3982b65ac0c5ce45655728dd743b
Vansh <vanshkatnor1737@gmail.com>
2026-08-27 18:22:49 +0530

    learning loops in java specifically for loop through different practice questions which also include use of previous concepts i had learned so far (Question 9)

---
+++ loops/forloop/Ques9.java
@@ -0,0 +1,27 @@
+// Now let's make the pattern actually think
+
+// For:
+
+// int n = 5;
+
+// produce:
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 4444
+// 333
+// 22
+// 1
+
+// But here's the restriction:
+
+// You must use ONE outer loop.
+
+package loops.forloop;
+
+public class Ques9 {
+        
+}
