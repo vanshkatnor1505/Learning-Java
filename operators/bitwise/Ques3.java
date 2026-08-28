e9763350585611cc9adaeaa7af41fe1bf387d503
Vansh <vanshkatnor1737@gmail.com>
2026-08-28 13:18:18 +0530

    Learning bitwise operators in java through various practice questions which also contain comcpets I previously learned (Practice question 3)

---
+++ operators/bitwise/Ques3.java
@@ -0,0 +1,36 @@
+// Bitwise Number Modifier
+
+// Start with:
+// int number = 73;
+
+// Your program must:
+// 1. Print the binary representation
+
+// 2. Check whether bit 3 is set
+// Print:
+// BIT 3 : SET
+// or
+// BIT 3 : NOT SET
+
+// 3. If bit 3 is NOT set → set it.
+
+// 4. If bit 3 IS set → clear it.
+// So the program should automatically decide whether to set or clear the bit.
+
+// 5. Toggle bit 6.
+
+// 6. Finally check bit 0 and report whether the final number is odd/even.
+
+// Expected structure:
+// ORIGINAL       : 73
+// BINARY        : xxxxxxx
+// BIT 3         : SET/NOT SET
+// AFTER BIT 3   : xxx
+// AFTER TOGGLE 6: xxx
+// FINAL PARITY  : ODD/EVEN
+
+package operators.bitwise;
+
+public class Ques3 {
+    
+}
