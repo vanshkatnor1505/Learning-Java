a1508e9d7768b23aaaf3a24652f313767633db6b
Vansh <vanshkatnor1737@gmail.com>
2026-08-30 19:55:01 +0530

    Learning Increment and Decrement operators in java through various practice questions which also contain comcpets I previously learned (Practice question 3)

---
+++ operators/incrementdecrement/Ques2.java
@@ -0,0 +1,30 @@
+// write a Java program that starts with:
+
+// int x = 10;
+// and performs this exact sequence:
+// int a = x++ + ++x;
+// int b = --x + x--;
+// int c = x++ + x++ + ++x;
+
+// Then print:
+// A : ?
+// B : ?
+// C : ?
+// FINAL X : ?
+
+package operators.incrementdecrement;
+
+public class Ques2 {
+    public static void main(String[] args) {
+        int x = 5;
+        int a = x++ + ++x;
+        int b = --x + x--;
+        int c = x++ + x++ + ++x;
+
+        System.out.println("A        : " + a);
+        System.out.println("B        : " + b);
+        System.out.println("C        : " + c);
+        System.out.println("FINAL X  : "  + x);
+
+    }
+}
