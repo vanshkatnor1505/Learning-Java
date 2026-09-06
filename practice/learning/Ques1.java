c884accefa9a5ebab896435e29088bf2350dd86f
Vansh <vanshkatnor1737@gmail.com>
2026-09-06 14:13:34 +0530

    done some more progress on the calculator added statements to handle meischevious entities find area of square with side of square equals to zero.

---
+++ practice/learning/Ques1.java
@@ -0,0 +1,12 @@
+package practice.learning;
+
+public class Ques1 {
+    public static void main(String[] args) {
+        int n = 25;
+        n%=10;
+
+        System.out.println(n);
+        
+
+    }
+}
