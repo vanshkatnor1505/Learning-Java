eb025a678efa7acee9766f6a33f5935ae8a1ec4e
Vansh <vanshkatnor1737@gmail.com>
2026-09-12 19:14:02 +0530

    added codes to new trigonometric functions

--- practice/learning/Ques1.java
+++ practice/learning/Ques1.java
@@ -4,7 +4,12 @@ public class Ques1 {
     public static void main(String[] args) {
         int n = 25;
         n%=10;
+        String hello = "hi";
 
+
+        if (hello.equals("hi") || hello.equals("hellow")) {
+            
+        }
         System.out.println(n);
         
 
