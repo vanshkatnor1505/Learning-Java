bec57eff42bd379a7b2e9a0a79a91bcf68bdeccf
Vansh <vanshkatnor1737@gmail.com>
2026-09-13 17:52:51 +0530

    Write a menu-driven Java program that uses a switch statement to perform
    addition, subtraction, multiplication, division and modulus
    
    on two numbers entered by the user.
    
    Use a loop so that the user can repeat the operation until they choose to exit.
    
    part 2

--- conditional/Ques6.java
+++ conditional/Ques6.java
@@ -3,17 +3,17 @@
 // on two numbers entered by the user. 
 // Use a loop so that the user can repeat the operation until they choose to exit.
 
-
 package conditional;
+
 import java.util.*;
 
 public class Ques6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int choice;
-        int result = 0;
+        double result, num1, num2;
 
-        do{
+        do {
 
             System.out.println("=========MENU=========");
             System.out.println("| 1.    Addition     |");
@@ -27,48 +27,55 @@ public class Ques6 {
             System.out.print("ENTER YOUR CHOICE : ");
             choice = sc.nextInt();
 
-
-
             if (1 <= choice && choice <= 5) {
                 System.out.print("ENTER FIRST NUMBER : ");
-                int num1 = sc.nextInt();
-                
+                num1 = sc.nextDouble();
+
                 System.out.print("ENTER SECOND NUMBER :");
-                int num2 = sc.nextInt();
+                num2 = sc.nextDouble();
 
                 switch (choice) {
                     case 1:
                         result = num1 + num2;
                         System.out.println("ADDITION IS : " + result);
+                        System.out.println("Thanks for using calculator");
                         break;
-                    
+
                     case 2:
-                        System.out.println("twoooo");
+                        result = num1 - num2;
+                        System.out.println("SUBTRACTION IS : " + result);
+                        System.out.println("Thanks for using calculator");
                         break;
 
                     case 3:
-                        System.out.println("threeeee");
+                        result = num1 * num2;
+                        System.out.println("MULTIPLICATION IS : " + result);
+                        System.out.println("Thanks for using calculator");
                         break;
 
                     case 4:
-                        System.out.println("fourrrrrr");
+                        result = num1 / num2;
+                        System.out.println("DIVISION IS : " + result);
+                        System.out.println("Thanks for using calculator");
                         break;
 
                     case 5:
-                        System.out.println("fiveeeee");
+                        result = num1 % num2;
+                        System.out.println("MODULUS IS : " + result);
+                        System.out.println("Thanks for using calculator");
 
                 }
-
 
             }
 
             else if (choice == 6) {
+                System.out.println("Thanks for using calculator");
                 System.out.println("EXITING THE CODE");
-            }
-            else{
+
+            } else {
                 System.out.println("Invailid choice");
             }
-        }while(choice != 6);
+        } while (choice != 6);
 
         sc.close();
     }
