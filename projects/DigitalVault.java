08bd6110bf7fd1019644a371c4fd6b6b5378f846
Vansh <vanshkatnor1737@gmail.com>
2026-09-16 17:35:21 +0530

    setting up project 2

---
+++ projects/DigitalVault.java
@@ -0,0 +1,47 @@
+// Digital Vault — Console Data Analyzer
+
+// The program starts with something like:
+
+// ========================================
+//         DIGITAL VAULT ANALYZER
+// ========================================
+// Enter number of elements: 10
+// Enter 10 numbers:
+// > 12
+// > -4
+// > 7
+// > 7
+// > 15
+// > -2
+// > 7
+// > 4
+// > 15
+// > 9
+
+// Then display a menu:
+// ========================================
+//               MAIN MENU
+// ========================================
+// 1. Display Array
+// 2. Reverse Array
+// 3. Sum & Average
+// 4. Largest & Smallest
+// 5. Even & Odd Analysis
+// 6. Search Element
+// 7. Frequency of Element
+// 8. Complete Frequency Report
+// 9. Second Largest / Second Smallest
+// 10. Maximum Subarray
+// 11. Exit
+// Choose an option:
+
+
+// The user can repeatedly choose options.
+
+
+
+package projects;
+
+public class DigitalVault {
+    
+}
